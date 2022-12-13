c38594f9d338a120a35a475d173bcffa860c96bd
Fernando Bade <bade.fernando@gmail.com>
2022-12-12 21:11:00 -0300

    Template base e fetch dos dentistas

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Card from "../Components/Card";
 
 const Home = () => {
+  const [dentist, setDentist] = useState([]);
 
   useEffect(() => {
     //Nesse useEffect, deverá ser obtido todos os dentistas da API
     //Armazena-los em um estado para posteriormente fazer um map
     //Usando o componente <Card />
+    try {
+      fetch("https://dhodonto.ctdprojetos.com.br/dentista")
+        .then((res) => res.json())
+        .then((data) => setDentist(data));
+    } catch (err) {
+      console.error(err);
+    }
   }, []);
 
   return (
     <>
       <h1>Home</h1>
       <div className="card-grid container">
-        <Card />
+        {dentist.length
+          ? dentist.map((dentist) => (
+              <Card {...dentist} key={dentist.matricula} />
+            ))
+          : null}
       </div>
     </>
   );
